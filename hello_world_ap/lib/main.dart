9ce640a11067ce069eedc3d8af6f8346599869ed
Obed Guzman Flores <230142@utxicotepec.edu.mx>
2026-09-08 15:34:18 -0600

    Practica 02: Primera Aplicacion de Flutter
    Programación de la pantalla contador

---
+++ hello_world_ap/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:hello_world_ap/presentation/screens/counter/counter_screen.dart';
+void main(){
+
+runApp(MyApp());
+ 
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CounterScreen()
+    );
+  }
+  
+
+}
