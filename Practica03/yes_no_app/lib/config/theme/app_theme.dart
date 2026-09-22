1d2361cddac30005f412c397b04ce1a63e2c0f88
Obed Guzman Flores <230142@utxicotepec.edu.mx>
2026-09-21 18:26:45 -0600

    Practica03 add:Seguimiento de la app
    Añadimiento de codigo nuevo a archivos creados

--- Practica03/yes_no_app/lib/config/theme/app_theme.dart
+++ Practica03/yes_no_app/lib/config/theme/app_theme.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-const Color _customColor = Color.fromARGB(255, 158, 55, 158);
+const Color _customColor = Color(0xFF49149F);
 
-const List<Color> colorTheme = [
+const List<Color> _colorThemes = [
   _customColor,
   Colors.blue,
   Colors.teal,
@@ -16,15 +16,14 @@ class AppTheme {
   final int selectedColor;
 
   AppTheme({this.selectedColor = 0})
-      : assert(
-          selectedColor >= 0 && selectedColor < colorTheme.length,
-          'El color debe estar entre 0 y ${colorTheme.length - 1}',
-        );
+      : assert(selectedColor >= 0 && selectedColor <= _colorThemes.length - 1,
+            'Colors must be between 0 and ${_colorThemes.length}');
 
   ThemeData theme() {
     return ThemeData(
-      useMaterial3: true,
-      colorSchemeSeed: colorTheme[selectedColor],
-    );
+        useMaterial3: true, 
+        colorSchemeSeed: _colorThemes[selectedColor],
+        // brightness: Brightness.dark
+      );
   }
 }
--- Practica03/yes_no_app/lib/presentation/Screens/chat/chat_screen.dart
+++ Practica03/yes_no_app/lib/presentation/Screens/chat/chat_screen.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+
+import 'package:yes_no_app/presentation/providers/chat_provider.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -8,34 +14,49 @@ class ChatScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-  leading: const Padding(
-    padding: EdgeInsets.all(4),
-    child: CircleAvatar(
-      backgroundImage: NetworkImage(
-        'https://tse3.mm.bing.net/th/id/OIP.3GwJqMnrOk8uSXwBGpd-KgHaEd?r=0&rs=1&pid=ImgDetMain&o=7&rm=3',
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAnRk4deYvshsw_gou8Ntnp8AY0bZvRlCaMP9A_i-nlQ&s'),
+          ),
+        ),
+        title: const Text('Ese wey'),
+        centerTitle: false,
       ),
-    ),
-  ),
-  title: const Text('Milky Chance'),
-),
-      body: const _ChatView(),
+      body: _ChatView(),
     );
   }
 }
 
 class _ChatView extends StatelessWidget {
-  const _ChatView();
-
   @override
   Widget build(BuildContext context) {
+    final chatProvider = context.watch<ChatProvider>();
+
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: ListView.builder(
-          itemCount: 100,
-          itemBuilder: (context, index) {
-            return const MyMessageBubble();
-          },
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+                  controller: chatProvider.chatScrollController,
+                    itemCount: chatProvider.messageList.length,
+                    itemBuilder: (context, index) {
+                      final message = chatProvider.messageList[index];
+                       
+                      return (message.fromWho == FromWho.hers)
+                          ? HerMessageBubble( message: message )
+                          : MyMessageBubble( message: message );
+                    })),
+
+           
+            MessageFieldBox(
+             
+              onValue: chatProvider.sendMessage,
+            ),
+          ],
         ),
       ),
     );
